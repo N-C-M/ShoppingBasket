70776e93294e26640f572ccda231ef43315d74b5
nmoore <nmoore@LT60121>
2016-06-03 16:45:56 +0100

    About UI re-designed.

--- ShoppingBasketForm/About.Designer.cs
+++ ShoppingBasketForm/About.Designer.cs
@@ -28,100 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.CPU = new System.Diagnostics.PerformanceCounter();
-            this.RAM = new System.Diagnostics.PerformanceCounter();
-            this.lblcpu = new System.Windows.Forms.Label();
-            this.lblram = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.pbCPU = new System.Windows.Forms.ProgressBar();
-            this.pbRAM = new System.Windows.Forms.ProgressBar();
             this.btnClose = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.CPU)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RAM)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // CPU
-            // 
-            this.CPU.CategoryName = "Processor";
-            this.CPU.CounterName = "% Processor Time";
-            this.CPU.InstanceName = "_Total";
-            // 
-            // RAM
-            // 
-            this.RAM.CategoryName = "Memory";
-            this.RAM.CounterName = "% Committed Bytes In Use";
-            // 
-            // lblcpu
-            // 
-            this.lblcpu.AutoSize = true;
-            this.lblcpu.Location = new System.Drawing.Point(21, 50);
-            this.lblcpu.Name = "lblcpu";
-            this.lblcpu.Size = new System.Drawing.Size(29, 13);
-            this.lblcpu.TabIndex = 0;
-            this.lblcpu.Text = "CPU";
-            // 
-            // lblram
-            // 
-            this.lblram.AutoSize = true;
-            this.lblram.Location = new System.Drawing.Point(21, 88);
-            this.lblram.Name = "lblram";
-            this.lblram.Size = new System.Drawing.Size(31, 13);
-            this.lblram.TabIndex = 1;
-            this.lblram.Text = "RAM";
-            // 
-            // pbCPU
-            // 
-            this.pbCPU.Location = new System.Drawing.Point(66, 40);
-            this.pbCPU.Name = "pbCPU";
-            this.pbCPU.Size = new System.Drawing.Size(167, 23);
-            this.pbCPU.TabIndex = 2;
-            // 
-            // pbRAM
-            // 
-            this.pbRAM.Location = new System.Drawing.Point(66, 78);
-            this.pbRAM.Name = "pbRAM";
-            this.pbRAM.Size = new System.Drawing.Size(167, 23);
-            this.pbRAM.TabIndex = 3;
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(104, 190);
+            this.btnClose.Location = new System.Drawing.Point(12, 223);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(260, 23);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Designed and developed by Nathan Moore";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Copyright 2016©";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Location = new System.Drawing.Point(94, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Shopping Basket";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ShoppingBasketForm.Properties.Resources.commerce_ecommerce_basket_shop;
+            this.pictureBox1.Location = new System.Drawing.Point(67, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Location = new System.Drawing.Point(14, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(254, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Software released under MyCodeBase Development";
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.pbRAM);
-            this.Controls.Add(this.pbCPU);
-            this.Controls.Add(this.lblram);
-            this.Controls.Add(this.lblcpu);
             this.MaximizeBox = false;
             this.Name = "About";
             this.Text = "About";
-            ((System.ComponentModel.ISupportInitialize)(this.CPU)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Diagnostics.PerformanceCounter RAM;
-        private System.Windows.Forms.Label lblcpu;
-        private System.Windows.Forms.Label lblram;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ProgressBar pbCPU;
-        private System.Windows.Forms.ProgressBar pbRAM;
-        protected System.Diagnostics.PerformanceCounter CPU;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ShoppingBasketForm/About.cs
+++ ShoppingBasketForm/About.cs
@@ -14,19 +14,7 @@ namespace ShoppingBasketForm
         public About()
         {
             InitializeComponent();
-
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            timer1.Interval = 100;
-            timer1.Start();
-        }
-
-        private void Timer1_Tick(System.Object sender, System.EventArgs e)
-        {
-            pbCPU.Value = (Int32)CPU.NextValue();
-            pbRAM.Value = (Int32)RAM.NextValue();
-
-            lblcpu.Text = pbCPU.Value + "%";
-            lblram.Text = pbRAM.Value + "%";
+            FormBorderStyle = FormBorderStyle.FixedSingle;  
         }
 
         private void btnClose_Click(object sender, EventArgs e)
